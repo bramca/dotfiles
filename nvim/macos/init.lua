4fc3c9df878547f90a18b5afe45d4731431ce7cb
Bram.Cautaerts <bram.cautaerts@ing.com>
2023-08-17 09:59:43 +0200

    changes in theme

--- nvim/macos/init.lua
+++ nvim/macos/init.lua
@@ -233,7 +233,7 @@ vim.wo.signcolumn = 'yes'
 
 -- Set colorscheme
 vim.o.termguicolors = true
-vim.cmd [[colorscheme tokyonight]]
+vim.cmd [[colorscheme tokyonight-night]]
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
