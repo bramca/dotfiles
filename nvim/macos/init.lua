583e40dbb906b6a0f5000dfd409d9cca2d3533b1
Bram.Cautaerts <bram.cautaerts@ing.com>
2023-08-18 14:55:19 +0200

    add colorizer to neovim config

--- nvim/macos/init.lua
+++ nvim/macos/init.lua
@@ -162,6 +162,7 @@ require('packer').startup(function(use)
   use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
   use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
   use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
+  use 'norcalli/nvim-colorizer.lua' -- Hex colorizer
 
   -- Fuzzy Finder (files, lsp, etc)
   use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
