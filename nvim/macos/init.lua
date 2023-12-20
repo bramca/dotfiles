64079eb6353bb6bb53e9b1f9fd09095efaeb567c
Bram.Cautaerts <bram.cautaerts@ing.com>
2023-12-20 10:20:15 +0100

    added javascript to treesitter

--- nvim/macos/init.lua
+++ nvim/macos/init.lua
@@ -392,7 +392,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'vim', 'http' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'vim', 'http', 'javascript' },
   ignore_install = { 'help' },
 
   highlight = { enable = true },
