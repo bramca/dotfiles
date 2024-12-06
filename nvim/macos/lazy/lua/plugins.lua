d28b464f886e858c33be307d7fc681c6f7daf6de
Bram.Cautaerts <bram.cautaerts@ing.com>
2024-12-06 16:28:21 +0100

    fix todo comments

--- nvim/macos/lazy/lua/plugins.lua
+++ nvim/macos/lazy/lua/plugins.lua
@@ -80,6 +80,24 @@ return {
   },
   { -- TODO comments
     'folke/todo-comments.nvim',
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local todo_comments = require("todo-comments")
+
+      -- set keymaps
+      local keymap = vim.keymap -- for conciseness
+
+      keymap.set("n", "]t", function()
+        todo_comments.jump_next()
+      end, { desc = "Next todo comment" })
+
+      keymap.set("n", "[t", function()
+        todo_comments.jump_prev()
+      end, { desc = "Previous todo comment" })
+
+      todo_comments.setup()
+    end,
   },
   { -- Workspace Diagnostics
     'folke/trouble.nvim',
