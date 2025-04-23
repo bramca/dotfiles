210a79041a6d4bee7e866fe3895b35320b1e9b53
bramca <bramcautaerts@gmail.com>
2025-04-23 17:52:16 +0200

    keep colorscheme

--- nvim/lazy/lua/plugins/colorscheme.lua
+++ nvim/lazy/lua/plugins/colorscheme.lua
@@ -1,32 +1,17 @@
 -- Github Theme
--- local M = {
---   'projekt0n/github-nvim-theme',
---   name = 'github-theme',
--- }
---
--- function M.config()
---   require('github-theme').setup({
---     options = {
---       dim_inactive = true,
---     }
---   })
---
---   vim.cmd.colorscheme "github_dark_default"
--- end
---
--- return M
-
--- Sonokai Theme
 local M = {
-  'sainnhe/sonokai',
+  'projekt0n/github-nvim-theme',
+  name = 'github-theme',
 }
 
 function M.config()
-  vim.g.sonokai_dim_inactive_windows = 1
-  vim.g.sonokai_style = "andromeda"
-  vim.g.sonokai_better_performance = 1
-  vim.g.sonokai_enable_italic = 1
-  vim.cmd.colorscheme "sonokai"
+  require('github-theme').setup({
+    options = {
+      dim_inactive = true,
+    }
+  })
+
+  vim.cmd.colorscheme "github_dark_default"
 end
 
 return M
