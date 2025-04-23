dab038d2b5c719ec895bead04e2e4e01ad6b9280
bramca <bramcautaerts@gmail.com>
2025-04-23 23:17:48 +0200

    update tmux and lualine

--- nvim/lazy/lua/plugins/lualine.lua
+++ nvim/lazy/lua/plugins/lualine.lua
@@ -5,8 +5,6 @@ local M = {
     options = {
       icons_enabled = true,
       theme = 'auto',
-      component_separators = { left = '|', right = '|' },
-      section_separators = { left = '', right = '' },
     },
     sections = {
       lualine_a = {'mode'},
