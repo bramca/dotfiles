5d4e7c4700f84b25b9d44b23a9438d48c50f9e17
bramca <bramcautaerts@gmail.com>
2025-05-09 06:19:43 +0200

    qol updates in nvim config

--- nvim/lazy/lua/plugins/nvim-cmp.lua
+++ nvim/lazy/lua/plugins/nvim-cmp.lua
@@ -3,6 +3,7 @@ local M = {
   "hrsh7th/nvim-cmp",
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-nvim-lsp-signature-help",
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
   },
@@ -38,6 +39,7 @@ function M.opts()
       { name = "lazydev" },
       { name = "nvim_lsp" },
       { name = "path" },
+      { name = "nvim_lsp_signature_help" },
     }, {
         { name = "buffer" },
       }),
