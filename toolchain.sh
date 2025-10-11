d0829629ca40660a09fea075124ffa948419ff38
bramca <bramcautaerts@gmail.com>
2025-10-11 20:08:41 +0200

    update toolchain

--- toolchain.sh
+++ toolchain.sh
@@ -7,7 +7,7 @@ curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/sh
 echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
 sudo chmod 644 /usr/share/keyrings/wezterm-fury.gpg
 sudo apt update
-sudo apt install brave-browesr stow tmux fd-find btop ripgrep fzf lua5.4 lua5.1 liblua5.4-dev liblua5.1-dev cargo npm wezterm
+sudo apt install brave-browesr stow tmux fd-find btop ripgrep fzf lua5.4 lua5.1 liblua5.4-dev liblua5.1-dev cargo npm wezterm tldr
 
 # install oh-my-bash
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-
