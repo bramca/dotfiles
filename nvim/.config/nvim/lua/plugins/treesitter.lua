db8932e07b5929684396c048f48b4e3c585b5d11
bramca <bramcautaerts@gmail.com>
2026-05-08 10:43:21 +0200

    nvim 0.11 treesitter support

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  -- TODO: update nvim to 0.12 for compatability with the latest version
+  commit = "90cd658",
   opts = {
     ensure_installed = {
       "c",
